5ae58c4a1a20ff46fe1cc9aff230cc511829a06c
4kiru <Mateusz.salawa@gmail.com>
2017-03-20 10:54:17 +0100

    14

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -1,11 +1,10 @@
 function init() {
 	document.addEventListener("online", onlinemode, false);
     document.addEventListener("offline", offlinemode, false);
-}
 
 
-function onlinemode(){
-    
+
+function onlinemode(){   
     alert("urządzenie jest online");
     networkinfo = navigator.connection;
     navigator.notification.alert(networkinfo);
@@ -14,6 +13,6 @@ function onlinemode(){
 function offlinemode(){
     alert("urzadzenie jest offline");
 }
-
+}
 
         
